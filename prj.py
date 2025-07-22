69c6f198d835b05ee3a6c0ae9206c35aeca75a61
Reet Shaw <reet shaw>
2025-07-22 18:00:13 +0530

    Initial commit

---
+++ prj.py
@@ -0,0 +1,27 @@
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+import matplotlib.pyplot as plt
+import seaborn as sns
+df = pd.read_csv("C:/Users/Reet Shaw/Downloads/archive/heart.csv")
+print(df.head())
+print(df.info())
+df_encoded = pd.get_dummies(df, drop_first=True)
+X = df_encoded.drop('HeartDisease', axis=1)
+y = df_encoded['HeartDisease']
+scaler = StandardScaler()
+X_scaled = scaler.fit_transform(X)
+X_train, X_test, y_train, y_test = train_test_split(X_scaled, y, test_size=0.2, random_state=42)
+model = RandomForestClassifier()
+model.fit(X_train, y_train)
+y_pred = model.predict(X_test)
+print("Accuracy:", accuracy_score(y_test, y_pred))
+print(confusion_matrix(y_test, y_pred))
+print(classification_report(y_test, y_pred))
+sns.heatmap(confusion_matrix(y_test, y_pred), annot=True, fmt='d', cmap='Blues')
+plt.title("Confusion Matrix")
+plt.xlabel("Predicted")
+plt.ylabel("Actual")
+plt.show()
